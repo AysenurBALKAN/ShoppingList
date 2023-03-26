01dd5ffefb2bb2da5cf128c7dfe78cfed023c639
AysenurBalkan <87125711+AysenurBalkan@users.noreply.github.com>
2023-03-26 15:04:08 +0300

    Add project files.

---
+++ ShoppingList/javascript.js
@@ -0,0 +1,54 @@
+﻿
+
+var icon = document.getElementById("icon");
+icon.onclick = function () {
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+        icon.src = "images/sun.png";
+    }
+    else {
+        icon.src = "images/moon.png";
+    }
+}
+var girilen, secilen;
+var eklenen, btntamamla, btnsil, sonuc;
+function liste_ekle() {
+    girilen = document.getElementById("txtListe").value;
+    if (girilen !="") {
+        eklenen = document.createElement("li");
+        document.getElementById("listeAlinacaklar").appendChild(eklenen);
+        eklenen.innerHTML = girilen;
+        btntamamla = document.createElement("img");
+        eklenen.appendChild(btntamamla);
+        btntamamla.setAttribute("src", "images/tamam.png");
+        btntamamla.setAttribute("id", "imgtamamla");
+        /* btntamamla.setAttribute("onclick", "tamamla()");*/
+        btntamamla.addEventListener("click", tamamla);
+        btnsil = document.createElement("img");
+        eklenen.appendChild(btnsil);
+        btnsil.setAttribute("src", "images/sil.png");
+        btnsil.setAttribute("id", "imgsil");
+        btnsil.addEventListener("click", sil);
+    }
+    else {
+        alert("Lütfen Boş Bırakmayınız");
+    }
+}
+function sil() {
+    secilen = event.currentTarget.parentNode;
+    secilen.remove();
+    document.getElementById("sonuc").innerHTML = "Seçiminiz Silindi";
+}
+
+function tamamla() {
+    eklenen = document.createElement("li");
+    document.getElementById("listeTamamlananlar").appendChild(eklenen);
+    secilen = event.currentTarget.parentNode;
+    secilen.childNodes[1].style.display = "none";
+    secilen.childNodes[2].setAttribute("onclick", "sil();");
+    eklenen.innerHTML = secilen.innerHTML;
+
+    sil();
+
+}
+
